41d7db4467a5c2155f1b1bd4b87f5ee71a2eca04
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-10-26 11:27:20 +0530

    logIn APIs implemented

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-200">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">DevTinder</a>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown dropdown-end">
@@ -13,7 +13,7 @@ const Navbar = () => {
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <div className="w-10 rounded-full">
+            <div className="w-10 rounded-full mr-2">
               <img
                 alt="Tailwind CSS Navbar component"
                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
